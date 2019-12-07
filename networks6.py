f4a10bef795edfb036419610c75c13ee044dfa37
hcguoup <56996691+hcguoup@users.noreply.github.com>
2019-12-06 23:24:07 -0800

    Update networks6.py

--- networks6.py
+++ networks6.py
@@ -50,11 +50,18 @@ def conv2d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
     if batchnorm:
         x = BatchNormalization()(x)
     x = Activation('relu')(x)
+    
+    # third layer
+    x = Conv2D(filters = n_filters, kernel_size = (kernel_size, kernel_size),\
+              kernel_initializer = 'he_normal', padding = 'same')(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation('relu')(x)
 
 
     return x
 
-def unet_2D(pretrained_weights = None,input_size = (256,256,3),n_filters = 64,batchnorm = True,dropout = 0.1):
+def unet_2D(pretrained_weights = None,input_size = (256,256,3),n_filters = 128,batchnorm = True,dropout = 0.1):
     inputs = Input(input_size)
     c1 = conv2d_block(inputs, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
     p1 = MaxPooling2D((2, 2))(c1)
@@ -72,30 +79,39 @@ def unet_2D(pretrained_weights = None,input_size = (256,256,3),n_filters = 64,ba
     p4 = MaxPooling2D((2, 2))(c4)
     p4 = Dropout(dropout)(p4)
     
-    c5 = conv2d_block(p4, n_filters = n_filters * 16, kernel_size = 3, batchnorm = batchnorm)
+    c5 = conv2d_block(p4, n_filters * 16, kernel_size = 3, batchnorm = batchnorm)
+    p5 = MaxPooling2D((2, 2))(c5)
+    p5 = Dropout(dropout)(p5)
+    
+    c6 = conv2d_block(p5, n_filters = n_filters * 32, kernel_size = 3, batchnorm = batchnorm)
     
     # Expansive Path
-    u6 = Conv2DTranspose(n_filters * 8, (3, 3), strides = (2, 2), padding = 'same')(c5)
-    u6 = concatenate([u6, c4])
-    u6 = Dropout(dropout)(u6)
-    c6 = conv2d_block(u6, n_filters * 8, kernel_size = 3, batchnorm = batchnorm)
-    
-    u7 = Conv2DTranspose(n_filters * 4, (3, 3), strides = (2, 2), padding = 'same')(c6)
-    u7 = concatenate([u7, c3])
+    u7 = Conv2DTranspose(n_filters * 16, (3, 3), strides = (2, 2), padding = 'same')(c6)
+    u7 = concatenate([u7, c5])
     u7 = Dropout(dropout)(u7)
-    c7 = conv2d_block(u7, n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
+    c7 = conv2d_block(u7, n_filters * 16, kernel_size = 3, batchnorm = batchnorm)
     
-    u8 = Conv2DTranspose(n_filters * 2, (3, 3), strides = (2, 2), padding = 'same')(c7)
-    u8 = concatenate([u8, c2])
+    u8 = Conv2DTranspose(n_filters * 8, (3, 3), strides = (2, 2), padding = 'same')(c7)
+    u8 = concatenate([u8, c4])
     u8 = Dropout(dropout)(u8)
-    c8 = conv2d_block(u8, n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
+    c8 = conv2d_block(u8, n_filters * 8, kernel_size = 3, batchnorm = batchnorm)
     
-    u9 = Conv2DTranspose(n_filters * 1, (3, 3), strides = (2, 2), padding = 'same')(c8)
-    u9 = concatenate([u9, c1])
+    u9 = Conv2DTranspose(n_filters * 4, (3, 3), strides = (2, 2), padding = 'same')(c8)
+    u9 = concatenate([u9, c3])
     u9 = Dropout(dropout)(u9)
-    c9 = conv2d_block(u9, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+    c9 = conv2d_block(u9, n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
+    
+    u10 = Conv2DTranspose(n_filters * 2, (3, 3), strides = (2, 2), padding = 'same')(c9)
+    u10 = concatenate([u10, c2])
+    u10 = Dropout(dropout)(u10)
+    c10 = conv2d_block(u10, n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
+    
+    u11 = Conv2DTranspose(n_filters * 1, (3, 3), strides = (2, 2), padding = 'same')(c10)
+    u11 = concatenate([u11, c1])
+    u11 = Dropout(dropout)(u11)
+    c11 = conv2d_block(u11, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
 
-    outputs = Conv2D(1,(1,1),activation = 'sigmoid')(c9)
+    outputs = Conv2D(1,(1,1),activation = 'sigmoid')(c11)
     model = Model(input = inputs, output = outputs)
 
     model.compile(optimizer = Adam() , loss = dice_coefficient_loss, metrics = [dice_coefficient])
